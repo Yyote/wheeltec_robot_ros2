92952f89b0281d5f7e87508c3e58e9da9f1a86e0
Andrey <yyootttaa@gmail.com>
2024-04-05 16:29:53 +0300

    teleop fix

--- src/launches/turn_on_launches/launch/teleop.launch.py
+++ src/launches/turn_on_launches/launch/teleop.launch.py
@@ -1,9 +1,7 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument, Node, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -20,7 +18,7 @@ def generate_launch_description():
         package="wheeltec_robot_rc",
         executable="turtlebot_teleop_keyboard",
         name="teleop",
-        namespace=f'{LaunchConfiguration('robot_name')}'
+        namespace=LaunchConfiguration('robot_name')
     ))
 
     return ld
