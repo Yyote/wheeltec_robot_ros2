e7acb26cc7d062e93dab286b9349901baedab284
Andrey <yyootttaa@gmail.com>
2024-04-09 17:54:20 +0300

    Added tf2 publisher for camera

---
+++ src/robot_models_tf2/src/tank_model.cpp
@@ -0,0 +1,55 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+
+
+using namespace std::chrono_literals;
+using geometry_msgs::msg::TransformStamped;
+
+class TankModelTFBroadcaster : public rclcpp::Node
+{
+    public:
+    TankModelTFBroadcaster() : Node("TankModelTFBroadcaster") // инициалзация полей
+    {
+        timer = this->create_wall_timer(100ms, std::bind(&TankModelTFBroadcaster::timer_callback, this));
+        broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    }
+
+
+    private:
+    rclcpp::TimerBase::SharedPtr timer;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> broadcaster;
+    
+    
+    void timer_callback() // Сама функция колбэка
+    {
+        TransformStamped transform;
+        transform.header.frame_id = "camera_color_optical_frame";
+        transform.header.stamp = this->get_clock()->now();
+        transform.child_frame_id = "_link";
+        
+        transform.transform.translation.x = 0;
+        transform.transform.translation.y = 0;
+        transform.transform.translation.z = 0;
+
+        transform.transform.rotation.x = 0;
+        transform.transform.rotation.y = 0;
+        transform.transform.rotation.z = 0;
+        transform.transform.rotation.w = 1;
+
+        broadcaster->sendTransform(transform);
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<TankModelTFBroadcaster>());
+    rclcpp::shutdown();
+    return 0;
+}
