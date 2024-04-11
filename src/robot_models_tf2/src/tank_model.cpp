e95c48840a5dd013879b014ed8f01d1f4152d8b4
Andrey <yyootttaa@gmail.com>
2024-04-11 15:50:40 +0300

    debugging

--- src/robot_models_tf2/src/tank_model.cpp
+++ src/robot_models_tf2/src/tank_model.cpp
@@ -37,12 +37,12 @@ class TankModelTFBroadcaster : public rclcpp::Node
         transform.transform.translation.y = 0;
         transform.transform.translation.z = 0;
 
-        transform.transform.rotation.x = 0;
-        transform.transform.rotation.y = 0;
+        transform.transform.rotation.x = 0.5;
+        transform.transform.rotation.y = 0.5;
         // transform.transform.rotation.z = -0.707;
-        transform.transform.rotation.z = 0;
+        transform.transform.rotation.z = 0.5;
         // transform.transform.rotation.w = 0.707;
-        transform.transform.rotation.w = 1;
+        transform.transform.rotation.w = -0.5;
 
         broadcaster->sendTransform(transform);
     }
