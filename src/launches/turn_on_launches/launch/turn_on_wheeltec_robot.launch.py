42b47ca08bf20edd81349e07b3c9d1d5d90d14a3
Andrey <yyootttaa@gmail.com>
2024-07-04 18:24:21 +0300

    1

--- src/launches/turn_on_launches/launch/turn_on_wheeltec_robot.launch.py
+++ src/launches/turn_on_launches/launch/turn_on_wheeltec_robot.launch.py
@@ -192,6 +192,7 @@ def generate_launch_description():
         
         # ld.add_action(cslam_launch)
         cslam_launch = GroupAction([
+                SetRemap(src="/color/camera_info", dst="/zed_node/rgb/camera_info"),
                 IncludeLaunchDescription(
                                 PythonLaunchDescriptionSource([os.path.join(
                                 get_package_share_directory('turn_on_launches'), ''),
