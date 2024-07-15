b91224b24b37f7b73a64164bc430f4e3d8e55f9e
Andrey <yyootttaa@gmail.com>
2024-07-15 19:21:20 +0300

    Minor additions

--- robot_info.sh
+++ robot_info.sh
@@ -3,3 +3,8 @@
 # export ROBOT_ID=0                         # Required!
 # export ROBOT_CAMERA_CAPABILITIES=astra_s  # Required!
 # export ROS_DOMAIN_ID=207
+
+echo Robot name = ${ROBOT_NAME}
+echo Robot id = ${ROBOT_ID}
+echo ROS DOMAIN ID = ${ROS_DOMAIN_ID}
+echo Robot info sourced.
