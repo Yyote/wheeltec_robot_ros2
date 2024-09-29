ab25eee6c72bed3bd9048d6512631fe89b313467
Andrey <yyootttaa@gmail.com>
2024-09-29 23:16:57 +0300

    fixed launch

--- src/launches/turn_on_launches/launch/turn_on_wheeltec_robot.launch.py
+++ src/launches/turn_on_launches/launch/turn_on_wheeltec_robot.launch.py
@@ -275,7 +275,7 @@ def generate_launch_description():
             package="tf2_ros",
             executable="static_transform_publisher",
             exec_name=f"{robot_name}_static_transform_odom_to_base_link",
-            arguments=["0", "0", "0", "0", "0", "0", f"{robot_id}/odom", f"{robot_name}/base_link"]
+            arguments=["0", "0", "0", "0", "0", "0", f"{robot_name}_odom", f"{robot_name}/base_link"]
         )
 
 
